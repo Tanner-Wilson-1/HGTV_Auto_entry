cf964549a4471e00b434845cfa3307ce4998ed05
Tanner Wilson <tannerw2013@gmail.com>
2023-01-22 11:41:17 -0800

    First Git commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+hgtv_url = 'https://www.hgtv.com/sweepstakes/hgtv-dream-home/sweepstakes'
+foodnetwork_url = 'https://www.foodnetwork.com/sponsored/sweepstakes/hgtv-dream-home-sweepstakes'
+all_email = ['tannerw2013@gmail.com', 'KevinBiedrzycki@gmail.com', 'meghannlkirby@gmail.com']
+HGTV_ngxFrame = 'ngxFrame230599'
+foodnetwork_ngxFrame = 'ngxFrame230603'
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+for emails in all_email:
+    browser.get(hgtv_url)
+    browser.maximize_window()
+
+    time.sleep(2)
+    browser.switch_to.frame(browser.find_element(By.ID, HGTV_ngxFrame))
+
+    time.sleep(2)
+    email_input = browser.find_element(By.ID, "xReturningUserEmail")
+    email_input.send_keys(emails)
+
+    time.sleep(2)
+    sign_up = browser.find_element(By.ID, "xCheckUser")
+    sign_up.click()
+
+    browser.switch_to.default_content()
+    time.sleep(2)
+    browser.switch_to.frame(browser.find_element(By.ID, HGTV_ngxFrame))
+    action = ActionChains(browser)
+    action.send_keys(Keys.TAB)
+    action.send_keys(Keys.ENTER)
+    action.perform()
+
+    time.sleep(10)
+
+    browser.get(foodnetwork_url)
+    browser.maximize_window()
+
+    time.sleep(2)
+    browser.switch_to.frame(browser.find_element(By.XPATH, foodnetwork_ngxFrame))
+
+    time.sleep(2)
+    email_input = browser.find_element(By.ID, "xReturningUserEmail")
+    email_input.send_keys(emails)
+
+    time.sleep(2)
+    sign_up = browser.find_element(By.ID, "xCheckUser")
+    sign_up.click()
+
+    browser.switch_to.default_content()
+    time.sleep(2)
+    browser.switch_to.frame(browser.find_element(By.XPATH, foodnetwork_ngxFrame))
+    action = ActionChains(browser)
+    action.send_keys(Keys.TAB)
+    action.send_keys(Keys.ENTER)
+    action.perform()
+
+    time.sleep(5)
+
+browser.quit()
